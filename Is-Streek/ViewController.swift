353941b560ad0d8a356b6f34a8bfe83f4e4b4559
FARABI DHARMA <74946238+farabidharma88@users.noreply.github.com>
2022-04-26 23:20:15 +0700

    Add files via upload

---
+++ Is-Streek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Is-Streek
+//
+//  Created by Farabi Dharma on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
